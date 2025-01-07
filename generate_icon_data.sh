c23889e57524900db1313ff0f21e346c7a5a008d
lejsson <ivar@ivarbjerling.com>
2025-01-07 22:59:30 +0100

    make shell script POSIX-compliant
    Changed from using echo statements to printf statements in generate_icon_data.sh for POSIX-compliance.

--- generate_icon_data.sh
+++ generate_icon_data.sh
@@ -1,29 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
 # Converts icons to hex arrays in c to embed them in the executable
 
 OUTPUT="icon_data.h"
 
-echo "/* This file contains the image data for the various icons */" > $OUTPUT;
+printf "/* This file contains the image data for the various icons */\n" > $OUTPUT;
 
 # meta will contain the offsets and sizes for the icons
 meta=""
 
 offset=0
 # Write one big array containing all images to $OUTPUT
-echo "const unsigned char icon_data[] = {" >> $OUTPUT
+printf "const unsigned char icon_data[] = {\n" >> $OUTPUT
 for icon in icons/*.png; do
-    echo "Embedding icon $icon"
-    echo "0x$(hexdump -e '16/1 "%02x " "\n"' $icon | xargs | sed -e 's/ /, 0x/g')," >> $OUTPUT
+    printf "Embedding icon $icon\n"
+    printf "0x$(hexdump -e '16/1 "%02x " "\n"' $icon | xargs | sed -e 's/ /, 0x/g'),\n" >> $OUTPUT
 
     name=${icon#"icons/"}
     name=${name%.*}
-    name=$(echo $name | sed -e 's/-/_/g')
+    name=$(printf "$name\n" | sed -e 's/-/_/g')
     size=$(du -b $icon | cut -f1)
     meta="${meta}\n #define icon_offset_$name $offset\n #define icon_size_$name $size"
     offset=$((offset + size))
 done
-echo "};" >> $OUTPUT
+printf "};\n" >> $OUTPUT
 
 # Put meta contents at the end (with line breaks)
-echo -e $meta >> $OUTPUT
+printf "%b" "$meta" >> $OUTPUT
